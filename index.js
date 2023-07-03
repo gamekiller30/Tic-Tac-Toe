cf75cf81a5a126f3bc0b11b9c760c9c57901adbe
gamekiller30 <tizian.grabert@gmail.com>
2023-07-03 23:32:43 +0200

    Added more complexity to Tic Tac Toe Logic

--- index.js
+++ index.js
@@ -1,167 +1,187 @@
+const b_box = document.querySelectorAll(".board-box");
+const win = document.querySelector(".winstatement");
+const reset_btn = document.querySelector(".reset-btn");
+let mytest =[]
+let MarkerArray = [];
+let Test = [];
+const Axes = [
+  //Vertikale
+[0, 1 ,2],
+[3, 4 , 5],
+[6, 7 ,8],
+
+//Horizontale
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+
+//Quer
+[0, 4, 8],
+[2 , 4, 6]
+
+];
+
 const Player = (name, marker) =>{
 
-const getName = () => name;
-const getMarker = () => marker;
-return {getName, getMarker};
-
-}
-
-
-
-const player1 = Player("Player1", "X");
-console.log(player1.getName());
-console.log(player1.getMarker());
-
-const player2 = Player("Player2", "O");
-console.log(player2.getName());
-console.log(player2.getMarker());
-
-
-
-const GameFlow = (roundbool) =>{
-
-let Boardarray = [];
-let MarkerArray = [];
-let playcount = 0;
-const reset_btn = document.querySelector(".reset-btn");
-let playerapp = document.querySelector(".playerwon");
-let child;
-let parent;
-
-
-
-const roundswitch = () =>{
-
-  if(roundbool === false){
-    console.log(roundbool + "");
-    roundbool = true;
-    playerapp.textContent = player1.getName() + " is playing now";
-    console.log(player1.getName() + " is playing now");
-    return player1.getMarker();
-  
-  }else{
-    console.log(roundbool + "");
-    roundbool = false;
-    playerapp.textContent = player2.getName() + " is playing now";
-    return player2.getMarker();
-  }
+  const getName = () => name;
+  const getMarker = () => marker;
+  return {getName, getMarker};
   
   }
 
-  const Reset = () =>{
-    Boardarray = [];
- MarkerArray = [];
- playcount = 0;
+  const GameFlow = () =>{
 
-const children = parent.children;
+    let roundbool = false;
 
-for(let i = 0; i < children.length; i++){
-children[i].textContent = "";
-  }
-}
+    const roundswitch = () =>{
+      
 
-reset_btn.addEventListener("click", Reset);
-
-  const PlayerWin = () =>{
-    if(roundswitch() != "X"){
-      playerapp.textContent = player1.getName() + " won";
-     
-    }else{
-      playerapp.textContent = player2.getName() + " won";
-     
-    }
-
-   
-
-    Reset();
-  }
-  
-  const Roundover = () => {
-    return "Round over, Restart the Game";
-  }
-  
-
-  /*
-  const SetMarker = function (e){
-
-   child.textContent = roundswitch();
-  }
- */
-
-  const checkWin = (i) =>{
-
-    //Vertikale
-if(((MarkerArray[0] == "X" && MarkerArray[1] == "X" && MarkerArray[2] == "X") || (MarkerArray[0] == "O" && MarkerArray[1] == "O" && MarkerArray[2] == "O")) || ((MarkerArray[3] == "X" && MarkerArray[4] == "X" && MarkerArray[5] == "X") || (MarkerArray[3] == "O" && MarkerArray[4] == "O" && MarkerArray[5] == "O")) || ((MarkerArray[6] == "X" && MarkerArray[7] == "X" && MarkerArray[8] == "X") || (MarkerArray[6] == "O" && MarkerArray[7] == "O" && MarkerArray[8] == "O"))){
-  console.log(PlayerWin());
-  console.log(Roundover());
-  //Horizontale
-}else if(((MarkerArray[0] == "X" && MarkerArray[3] == "X" && MarkerArray[6] == "X") || (MarkerArray[0] == "O" && MarkerArray[3] == "O" && MarkerArray[6] == "O")) || ((MarkerArray[1] == "X" && MarkerArray[4] == "X" && MarkerArray[7] == "X") || (MarkerArray[1] == "O" && MarkerArray[4] == "O" && MarkerArray[7] == "O")) || ((MarkerArray[2] == "X" && MarkerArray[5] == "X" && MarkerArray[8] == "X") || (MarkerArray[2] == "O" && MarkerArray[5] == "O" && MarkerArray[8] == "O"))){
-  console.log(PlayerWin());
-  console.log(Roundover());
-}else if(((MarkerArray[0] == "X" && MarkerArray[4] == "X" && MarkerArray[8] == "X") || (MarkerArray[0] == "O" && MarkerArray[4] == "O" && MarkerArray[8] == "O")) || ((MarkerArray[2] == "X" && MarkerArray[4] == "X" && MarkerArray[6] == "X") || (MarkerArray[2] == "O" && MarkerArray[4] == "O" && MarkerArray[6] == "O"))){
-  console.log(PlayerWin());
-  console.log(Roundover());
-}
-
-console.log(MarkerArray);
-console.log("Position " + i + " besitzt Wert "+ MarkerArray[i]);
-
-    }
-  
-
-const drawBoard = () =>{
-
- parent = document.querySelector(".board-grid");
-
-for(let i = 0; i < 9; i++){
-
-  child = document.createElement("div");
-  child.classList.add("board-box");
-  child.addEventListener("click" , function (e){
-
-
-      if(MarkerArray[i] != "X" && MarkerArray[i] != "O"){
-
-
-        MarkerArray[i] = roundswitch();
-        e.target.textContent =  MarkerArray[i];
-        console.log("Marker " + MarkerArray[i])
-     
-        playcount++;
-        console.log(playcount)
-        
+      if(roundbool === false){
+        console.log(roundbool + "");
+        roundbool = true;
+        //playerapp.textContent = player1.getName() + " is playing now";
+        console.log(player1.getName() + " is playing now");
+        return player1.getMarker();
+      
       }else{
-        console.log("Position already in use.");
-        
+        console.log(roundbool + "");
+        roundbool = false;
+        //playerapp.textContent = player2.getName() + " is playing now";
+        return player2.getMarker();
+      }
+      
       }
 
-      console.log("Marker " + MarkerArray[i])
-      console.log("Marker " + MarkerArray)
 
- 
+      const checkWin = () =>{
 
-  checkWin(i)
+        let num = 0;
 
-  if(playcount == 9){
-    console.log(Roundover() + " it was a Tie");
-    playerapp.textContent = "It was a Tie";
-    Reset();
-   }
+        for(let i = 0; i < Axes.length; i++){
+        
+         num = 0;
+      
+          console.log(Axes[i] + " Axes at Pos " + i)
+          console.log("----------------------------")
+      
+          for(let k = 0; k < Axes[i].length; k++){
+      
+            if(MarkerArray[Axes[i][k]] == "X"){
 
-  });
+              /*Mark the Rows that WON logic
+         if(num === 3){
+              Test.push(Axes[i][k]);
+           }
+*/
 
-  parent.appendChild(child);
-  console.log(i + " child created");
-  Boardarray.push(child);
+              num++;
+
+            }else if(MarkerArray[Axes[i][k]] == "O"){
+              num--;
+            }
+            console.log("num is " + num)
+      
+            announceWin(num);
+        
+         
+            console.log(Axes[i][k] + " Pos with new Tech is Marker " + MarkerArray[Axes[i][k]] + " in Marker Arr")
+          
+            
+          }
+          console.log("----------------------------")
+        }
+      }
+    
+const announceWin = (num) =>{
+
+  if(num == 3){
+   
+    /*Mark the Rows that WON logic  
+Test = [
+
+];*/
+    console.log(" X WIN")
+      /*Mark the Rows that WON logic
+    for(let i = 0; i < 3; i++){
+      mytest[Test[0][i]].style.backgroundColor = "green";
+    }
+        console.log(Test)
+    */
+    console.log(mytest)
+    win.textContent = "X won";
+  }else if(num == -3){
+    win.textContent = "O won";
+  }
+
+}
+
+const GameReset = () =>{
+  num = 0;
+  roundbool = false;
+  MarkerArray = [];
+
+  b_box.forEach(item  =>{
+  
+    item.textContent = "";
+    console.log(item + "removed")
+  })
+
 }
 
 
+return {checkWin, roundswitch, GameReset};
+
+  }
+  
+  
+  
+  const player1 = Player("Player1", "X");
+  
+  /*console.log(player1.getName());
+  console.log(player1.getMarker());*/
+  
+  const player2 = Player("Player2", "O");
+  
+  /*console.log(player2.getName());
+  console.log(player2.getMarker()); */
+
+  
+
+const gameflow = GameFlow();
+
+
+let i = 0;
+b_box.forEach(item  =>{
+
+  item.value = i;
+  item.addEventListener("click", SetMark);
+console.log(item.value);
+i++;
+
+})
+
+
+function SetMark (e) {
+
+  e.target.textContent = gameflow.roundswitch();
+  MarkerArray[e.target.value] = e.target.textContent;
+  mytest[e.target.value] = e.target;
+  console.log(mytest)
+  console.log(MarkerArray[e.target.value]);
+  console.log(e.target.value);
+  console.log(MarkerArray)
+  //console.log("TEST "+ Test)
+  gameflow.checkWin();
+
 }
 
-return {drawBoard};
-}
 
 
-const startgame = GameFlow(false);
-startgame.drawBoard();
+reset_btn.addEventListener("click", () =>{
+  gameflow.GameReset()
+});
+
+
+
+
 
